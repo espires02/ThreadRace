bf48abdb9c033d1229a412edfe46d1e192717d5b
eleaz <eleaz@DESKTOP-ALM5Q8D>
2024-04-26 14:03:00 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿namespace threadRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read, Set Go...");
+            int t1Location = 0;
+            int t2Location = 0;
+            int t3Location = 0;
+            int t4Location = 0;
+            int t5Location = 0;
+
+            //Creating Threads
+            Thread t1 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Speedy Gonzales";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t1Location);
+                }
+
+            });
+            Thread t2 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Road Runner";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t2Location);
+
+                }
+            });
+            Thread t3 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Ricky Bobby";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t3Location);
+                }
+            });
+            Thread t4 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Speed Racer";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t4Location);
+                }
+            });
+            Thread t5 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Flynn Rider";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t5Location);
+                }
+            });
+
+            t3.Priority = ThreadPriority.AboveNormal;
+
+            //Executing the methods
+            t1.Start();
+            t2.Start();
+            Console.WriteLine("Race has ended");
+        }
+        static void MoveIt(ref int location)
+        {
+            location++;
+            Console.WriteLine($"{Thread.CurrentThread.Name} location={location}");
+            if (location >= 100)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} is the winner");
+                return;
+            }
+        }
+
+    }
+}
